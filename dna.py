c73448410a4329389d4e4cc64661067febc905bf
Julian <jz.quark@gmail.com>
2019-07-03 20:53:30 +0300

    Add files via upload
    http://rosalind.info/problems/dna/

---
+++ dna.py
@@ -0,0 +1,17 @@
+def getCountACGT(strin):
+	acgt = {"A": 0, "C": 0, "G": 0, "T": 0}
+
+	for it in strin:
+		acgt[it] += 1;
+
+	return acgt
+
+
+path = input("file path: ")
+
+if path:
+	f = open(path, "r")
+	text = f.read()
+	result = getCountACGT(text) 
+
+	print(str(result["A"]) + " " + str(result["C"]) + " " + str(result["G"]) + " " + str(result["T"]))
