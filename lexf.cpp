89ef01aba405979abff7f984ef78a816b323ae6c
Julian <jz.quark@gmail.com>
2019-07-10 19:32:21 +0300

    Add files via upload
    http://rosalind.info/problems/lexf/

---
+++ lexf.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <limits>
+#include <iomanip>
+#include <bitset>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <ctime>
+
+#define FOR(i,a,b) for (int i = a; i < b; i++)
+#define FO(i,n) FOR(i,0,n)
+#define pb push_back
+#define VI vector<int>
+#define VB vector<bool>
+#define INF 1E9
+
+using namespace std;
+
+vector<char> v;
+VB used;
+vector<char> z;
+
+void print()
+{
+    FO(i, v.size())
+     cout<<v[i];
+    
+    cout<<endl;
+}
+
+void perm(int idx, int n)
+{
+    if (idx == n)
+    {
+        print();
+        return;
+    }
+    
+    FO(i, z.size())
+    {
+        v[idx] = z[i];
+        perm(idx + 1, n);
+    }
+        
+}
+
+int main()
+{
+    int n;
+    string str;
+    
+    ifstream ifs("input.txt");
+    
+    getline(ifs, str);
+    
+    FO(i, str.length())
+    {
+        if (str[i] != ' ')
+            z.pb(str[i]);
+    }
+    
+    ifs >> n;
+
+    v = vector<char>(n);
+    used = vector<bool>(n + 1, false);
+    perm(0, n);
+    
+    
+return 0;
+    
+}
